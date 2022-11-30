95e85cc8393b005c5c004a259ea5c525ff5cec15
Nikolai Breimon <nikolai.solo.b@gmail.com>
2022-11-30 14:20:07 -0600

    added skeleton of attacking, just need to implement it.

---
+++ content/events/die.h
@@ -0,0 +1,14 @@
+#pragma once
+
+#include "event.h"
+
+class Actor;
+class Die : public Event {
+public:
+    Die(Actor& actor);
+    void execute(Engine& engine) override;
+
+
+private:
+    Actor& actor;
+};
